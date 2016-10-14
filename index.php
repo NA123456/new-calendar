ec44f3029eccf82f65fa3f2056163df400e52db5
NA123456 <nadazidan75@gmail.com>
2016-10-14 00:36:56 -0700

    -

--- index.php
+++ index.php
@@ -181,13 +181,10 @@
           <button type="button" class="close" data-dismiss="modal">&times;</button>
           <h4 class="modal-title">Edit Event</h4>
         </div>
-        <div class="modal-body">
+        <div class="modal-body nEvent">
         
         <!-- Form content -->
-         <form class="event">
-
-		
-        </form>
+      
         <!-- end form -->
       </div>
     </div>
--- retrive.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "events.php";
-
-$e = new events();
-
-$e->retriveEvent($_POST['e']);
-?>
